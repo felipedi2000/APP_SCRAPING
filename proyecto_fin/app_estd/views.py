0154131f26b1201f993f2d357671b595527c8e87
mzambrano <mzambrano@homecenter.co>
2024-06-18 16:10:36 -0500

    Ingreso de data por posicion

--- proyecto_fin/app_estd/views.py
+++ proyecto_fin/app_estd/views.py
@@ -16,7 +16,6 @@ def ver_nosotros(request):
     return render(request,"nosotros.html")
 
 #por jugador
-
 def defensa_central(request):
     id = request.GET.get('id', '')
     print("Mensaje recibido:", id)
@@ -27,55 +26,102 @@ def defensa_central(request):
     print(f"jugador:{jugador}")
     return render(request, 'jd_defensa_central.html',{'jugador': jugador})
 
-
 def delantero_centro(request):
     id = request.GET.get('id', '')
-    # recibir los datos de id y insertarlos en el html
-    # name = request.GET.get('name', '')
     print("Mensaje recibido:", id)
-    return render(request, 'jd_delantero_centro.html')
+    jugador_id = int(id)
+    jugador = jugadores_collection.find_one({"id": jugador_id},{
+        "Name": 1, "Nationality": 1, "Age": 1, "team": 1, "Number": 1, "MarkeValue": 1, "Position": 1, "goals": 1, "yellowCards": 1, "redCards": 1, "minutesPlayed": 1, "assists": 1,"successfulDribbles": 1,"totalShots": 1,"bigChancesMissed": 1, "offsides": 1,"shotsOnTarget": 1,"aerialDuelsWon": 1
+    })
+    print(f"jugador:{jugador}")
+    return render(request, 'jd_delantero_centro.html',{'jugador': jugador})
 
 def extremo_derecho(request):
     id = request.GET.get('id', '')
     print("Mensaje recibido:", id)
-    return render(request, 'jd_extremo_derecho.html')
+    jugador_id = int(id)
+    jugador = jugadores_collection.find_one({"id": jugador_id},{
+        "Name": 1, "Nationality": 1, "Age": 1, "team": 1, "Number": 1, "MarkeValue": 1, "Position": 1, "goals": 1, "yellowCards": 1, "redCards": 1, "minutesPlayed": 1, "assists": 1,"successfulDribbles": 1,"totalShots": 1,"bigChancesCreated": 1, "shotsOnTarget": 1,"keyPasses": 1,"accurateCrosses": 1,"accuratePassesPercentage": 1, "bigChancesMissed":1
+    })
+    print(f"jugador:{jugador}")
+    return render(request, 'jd_extremo_derecho.html',{'jugador': jugador})
 
 def extremo_izquierdo(request):
     id = request.GET.get('id', '')
     print("Mensaje recibido:", id)
-    return render(request, 'jd_extremo_izquierdo.html')
+    jugador_id = int(id)
+    jugador = jugadores_collection.find_one({"id": jugador_id},{
+        "Name": 1, "Nationality": 1, "Age": 1, "team": 1, "Number": 1, "MarkeValue": 1, "Position": 1, "goals": 1, "yellowCards": 1, "redCards": 1, "minutesPlayed": 1, "assists": 1,"successfulDribbles": 1,"totalShots": 1,"bigChancesCreated": 1, "shotsOnTarget": 1,"keyPasses": 1,"accurateCrosses": 1,"accuratePassesPercentage": 1, "bigChancesMissed":1
+    })
+    print(f"jugador:{jugador}")
+    return render(request, 'jd_extremo_izquierdo.html',{'jugador': jugador})
 
 def lateral_derecho(request):
     id = request.GET.get('id', '')
     print("Mensaje recibido:", id)
-    return render(request, 'jd_lateral_derecho.html')
+    jugador_id = int(id)
+    jugador = jugadores_collection.find_one({"id": jugador_id},{
+        "Name": 1, "Nationality": 1, "Age": 1, "team": 1, "Number": 1, "MarkeValue": 1, "Position": 1, "goals": 1, "yellowCards": 1, "redCards": 1, "minutesPlayed": 1, "assists": 1,"interceptions": 1,"clearances": 1,"accuratePassesPercentage": 1, "accurateCrosses": 1,"successfulDribbles": 1,"keyPasses": 1
+    })
+    print(f"jugador:{jugador}")
+    return render(request, 'jd_lateral_derecho.html',{'jugador': jugador})
 
 def lateral_izquierdo(request):
     id = request.GET.get('id', '')
     print("Mensaje recibido:", id)
-    return render(request, 'jd_lateral_izquierdo.html')
+    jugador_id = int(id)
+    jugador = jugadores_collection.find_one({"id": jugador_id},{
+        "Name": 1, "Nationality": 1, "Age": 1, "team": 1, "Number": 1, "MarkeValue": 1, "Position": 1, "goals": 1, "yellowCards": 1, "redCards": 1, "minutesPlayed": 1, "assists": 1,"interceptions": 1,"clearances": 1,"accuratePassesPercentage": 1, "accurateCrosses": 1,"successfulDribbles": 1,"keyPasses": 1
+    })
+    print(f"jugador:{jugador}")
+    return render(request, 'jd_lateral_izquierdo.html',{'jugador': jugador})
 
 def mediapunta(request):
     id = request.GET.get('id', '')
     print("Mensaje recibido:", id)
-    return render(request, 'jd_mediapunta.html')
+    jugador_id = int(id)
+    jugador = jugadores_collection.find_one({"id": jugador_id},{
+        "Name": 1, "Nationality": 1, "Age": 1, "team": 1, "Number": 1, "MarkeValue": 1, "Position": 1, "goals": 1, "yellowCards": 1, "redCards": 1, "minutesPlayed": 1, "assists": 1,"successfulDribbles": 1,"totalShots": 1,"bigChancesCreated": 1, "shotsOnTarget": 1,"keyPasses": 1,"accuratePassesPercentage": 1, "bigChancesMissed":1
+    })
+    print(f"jugador:{jugador}")
+    return render(request, 'jd_mediapunta.html',{'jugador': jugador})
 
 def mediocampista_ofensivo(request):
     id = request.GET.get('id', '')
     print("Mensaje recibido:", id)
-    return render(request, 'jd_mediocentro_ofensivo.html')
+    jugador_id = int(id)
+    jugador = jugadores_collection.find_one({"id": jugador_id},{
+        "Name": 1, "Nationality": 1, "Age": 1, "team": 1, "Number": 1, "MarkeValue": 1, "Position": 1, "goals": 1, "yellowCards": 1, "redCards": 1, "minutesPlayed": 1, "assists": 1,"successfulDribbles": 1,"totalShots": 1,"bigChancesCreated": 1, "shotsOnTarget": 1,"keyPasses": 1,"accuratePassesPercentage": 1, "bigChancesMissed":1
+    })
+    print(f"jugador:{jugador}")
+    return render(request, 'jd_mediocentro_ofensivo.html',{'jugador': jugador})
 
 def pivote(request):
     id = request.GET.get('id', '')
     print("Mensaje recibido:", id)
-    return render(request, 'jd_pivote.html')
+    jugador_id = int(id)
+    jugador = jugadores_collection.find_one({"id": jugador_id},{
+        "Name": 1, "Nationality": 1, "Age": 1, "team": 1, "Number": 1, "MarkeValue": 1, "Position": 1, "goals": 1, "yellowCards": 1, "redCards": 1, "minutesPlayed": 1, "assists": 1,"successfulDribbles": 1,"totalShots": 1,"bigChancesCreated": 1, "shotsOnTarget": 1,"keyPasses": 1,"accuratePassesPercentage": 1, "bigChancesMissed":1
+    })
+    print(f"jugador:{jugador}")
+    return render(request, 'jd_pivote.html',{'jugador': jugador})
 
 def portero(request):
     id = request.GET.get('id', '')
     print("Mensaje recibido:", id)
-    return render(request, 'jd_portero.html')
+    jugador_id = int(id)
+    jugador = jugadores_collection.find_one({"id": jugador_id},{
+        "Name": 1, "Nationality": 1, "Age": 1, "team": 1, "Number": 1, "MarkeValue": 1, "Position": 1, "goals": 1, "yellowCards": 1, "redCards": 1, "minutesPlayed": 1, "assists": 1,"goalsConcededOutsideTheBox": 1,"saves": 1,"totalDuelsWon": 1, "goalsConcededInsideTheBox": 1,"savedShotsFromInsideTheBox": 1,"savedShotsFromOutsideTheBox": 1, "highClaims":1
+    })
+    print(f"jugador:{jugador}")
+    return render(request, 'jd_portero.html',{'jugador': jugador})
 
 def mediocentro(request):
     id = request.GET.get('id', '')
     print("Mensaje recibido:", id)
-    return render(request, 'jd_mediocentro.html')
+    jugador_id = int(id)
+    jugador = jugadores_collection.find_one({"id": jugador_id},{
+        "Name": 1, "Nationality": 1, "Age": 1, "team": 1, "Number": 1, "MarkeValue": 1, "Position": 1, "goals": 1, "yellowCards": 1, "redCards": 1, "minutesPlayed": 1, "assists": 1,"interceptions": 1,"clearances": 1,"accurateLongBalls": 1, "accuratePassesPercentage": 1
+    })
+    print(f"jugador:{jugador}")
+    return render(request, 'jd_mediocentro.html',{'jugador': jugador})
